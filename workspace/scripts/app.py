6d3b7db79acdd145e9751ebc47be471ad2473404
Chibuike Okpaluba <co607@live.mdx.ac.uk>
2018-12-13 22:06:46 +0000

    added second GUI

--- workspace/scripts/app.py
+++ workspace/scripts/app.py
@@ -33,9 +33,154 @@ from prediction.shallownet import ShallowNet_Cards
 from prediction.lenet      import LeNet_Cards
 from prediction.minivggnet import MiniVGGNet_Cards
 
+class BaxterImageViewer(QWidget):
+    def __init__(self, parent=None):
+        super(BaxterImageViewer, self).__init__(parent)
+
+        self.baxter_app = None
+
+        self.camera_image = None
+        self.focus_image = None
+
+        self.closing = False
+
+        self.__capture_device = None
+        self.__capture_timer  = QTimer(self)
+        self.__capture_timer.timeout.connect(self.handle_camera_update)
+        self.__capture_timer.setInterval(100)
+
+        self.init_UI()
+        self.show()
+    
+    def closeEvent(self, event):
+        self.closing = True
+
+        if not self.baxter_app.closing:
+            self.baxter_app.quit_app()
+
+        self.disable_camera()
+        event.accept()
+    
+    def keyPressEvent(self, event):
+        key = event.key()
+
+        if key in [Qt.Key_Q, Qt.Key_Escape]:
+            self.quit_app()
+
+    def quit_app(self):
+        super(BaxterImageViewer, self).close()
+    
+    def init_UI(self):
+        self.setWindowTitle("Image Viewer")
+        self.setWindowModality(Qt.ApplicationModal)
+        self.setFixedSize(525, 225)
+        self.move(100, 400)
+
+        self.camera_view_scene = QGraphicsScene()
+        self.camera_view_view  = QGraphicsView(self.camera_view_scene, self)
+        self.camera_view_view.resize(375, 225)
+        self.camera_view_view.move(150, 0)
+
+        self.focus_view_scene = QGraphicsScene()
+        self.focus_view_view  = QGraphicsView(self.focus_view_scene, self)
+        self.focus_view_view.resize(150, 225)
+    
+    def set_baxter_app(self, baxter_app):
+        self.baxter_app = baxter_app
+    
+    def is_frame_valid(self, frame, nchannel=3):
+        if frame is None:
+            return False
+        
+        try:
+            if len(frame.shape) != nchannel:
+                return False
+        except:
+            return False
+        
+        return True
+    
+    def handle_camera_update(self):
+        if self.__capture_device is None:
+            return
+        
+        # TODO: handle error reading from device
+
+        _, frame = self.__capture_device.read()
+
+        if not self.is_frame_valid(frame):
+            return
+        
+        # for cropped frame
+        nh, nw = 320, 207; offset = 216
+        
+        self.camera_image = frame.copy()
+        self.focus_image = frame[0:nh, offset:offset+nw, :]
+
+        return self.render()
+    
+    def render(self):
+        if not self.is_frame_valid(self.camera_image):
+            return
+        
+        img = cv2.cvtColor(self.camera_image, cv2.COLOR_BGR2RGB)
+        im_height, im_width, byte_val = img.shape
+
+        ci_qimage = QImage(img.data, im_width, im_height, byte_val * im_width, QImage.Format_RGB888)
+        ci_pix_map = QPixmap.fromImage(ci_qimage)
+
+        self.camera_view_scene.clear()
+        self.camera_view_scene.addPixmap(ci_pix_map)
+        self.camera_view_view.fitInView(QRectF(0,0,im_width, im_height), Qt.KeepAspectRatio)
+        self.camera_view_scene.update()
+
+        
+        
+        
+        if not self.is_frame_valid(self.focus_image):
+            return
+        
+        img = cv2.cvtColor(self.focus_image, cv2.COLOR_BGR2RGB)
+        im_height, im_width, byte_val = img.shape
+
+        ci_qimage = QImage(img.data, im_width, im_height, byte_val * im_width, QImage.Format_RGB888)
+        ci_pix_map = QPixmap.fromImage(ci_qimage)
+
+        self.focus_view_scene.clear()
+        self.focus_view_scene.addPixmap(ci_pix_map)
+        self.focus_view_view.fitInView(QRectF(0,0,im_width, im_height), Qt.KeepAspectRatio)
+        self.focus_view_scene.update()
+    
+    def enable_camera(self, camera_id):
+        if self.__capture_device is not None:
+            self.__capture_device.release()
+        
+        try:
+            self.__capture_device = cv2.VideoCapture(camera_id)
+
+            if not self.__capture_device.isOpened():
+                raise IOError("Could not open image capture device with id {}".format(camera_id))
+            
+            self.__capture_timer.start()
+        except:
+            self.baxter_app.show_message(msg="Could not access your camera")
+            self.__capture_device = None
+    
+    def disable_camera(self):
+        self.__capture_timer.stop()
+        if self.__capture_device is not None:
+            self.__capture_device.release()
+
+            del self.__capture_device
+            self.__capture_device = None
+
+    
 class BaxterApp(QWidget):
     def __init__(self, parent=None):
         super(BaxterApp, self).__init__(parent)
+
+        self.closing = False
+        self.baxter_image_viewer = None
 
         __name_pre_formater = lambda n : "{}/{}_icons.png".format(_BA_SUIT_ICONS_LOCATION, n)
         self.icons = {
@@ -62,6 +207,11 @@ class BaxterApp(QWidget):
         reply = QMessageBox.question(self, "Quiting", "Do you want to exit?", QMessageBox.Yes, QMessageBox.No)
 
         if reply == QMessageBox.Yes:
+            self.closing = True
+
+            if not self.baxter_image_viewer.closing:
+                self.baxter_image_viewer.quit_app()
+
             event.accept()
         else:
             event.ignore()
@@ -210,6 +360,9 @@ class BaxterApp(QWidget):
     def quit_app(self):
         super(BaxterApp, self).close()
     
+    def set_baxter_image_viewer(self, baxter_image_viewer):
+        self.baxter_image_viewer = baxter_image_viewer
+    
     def show_message(self, title="Warning!", msg="Could not read image?"):
         QMessageBox.question(self, title, msg, QMessageBox.Ok, QMessageBox.Ok)
     
@@ -230,6 +383,8 @@ class BaxterApp(QWidget):
             return
         else:
             self.camera_id = cam_id
+        
+        self.baxter_image_viewer.enable_camera(self.camera_id)
     
     def card_01_select_btn_callback(self):
         if not self.card_01_select_btn.isChecked():
@@ -273,5 +428,11 @@ class BaxterApp(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = BaxterApp()
+    
+    biv = BaxterImageViewer()
+    ba  = BaxterApp()
+
+    biv.set_baxter_app(ba)
+    ba.set_baxter_image_viewer(biv)
+
     sys.exit(app.exec_())
